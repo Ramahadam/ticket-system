a20bc92ef447c9ead47ace5ca28d611abd3d091a
RamahAdam <ramy.adam33@gmail.com>
2024-12-24 00:00:17 +0400

    update deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: '/ticket-system/',
   plugins: [react()],
   define: {
     global: "globalThis", // Define globalThis as an empty object
