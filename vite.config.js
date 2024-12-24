9b1497d762e9774e8fae89bf37e0baa4fa972c30
RamahAdam <ramy.adam33@gmail.com>
2024-12-24 20:27:03 +0400

    fixing the load recrouces error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ticket-system/',
+  // base: '/ticket-system/',
+  base: '/',
   plugins: [react()],
   define: {
     global: "globalThis", // Define globalThis as an empty object
