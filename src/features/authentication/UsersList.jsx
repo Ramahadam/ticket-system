508b921c33d3a88d1419ee98c060988181e194a3
RamahAdam <ramy.adam33@gmail.com>
2025-01-20 14:41:32 +0400

    Refactor authentication components by removing old styles and implementing new Members and UsersList components with updated CSS

--- src/features/authentication/UsersList.jsx
+++ src/features/authentication/UsersList.jsx
@@ -1,71 +1,91 @@
+// import { useUsers } from '../../context/UsersContext';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useUsers } from '../../context/UsersContext';
-import styles from './UsersList.module.css';
+import Button from '../../ui/Button';
 import {
-  faClipboardUser,
-  faPen,
-  faPenToSquare,
-  faTrashCan,
+  faAddressBook,
+  faEdit,
+  faTrash,
+  faUserEdit,
 } from '@fortawesome/free-solid-svg-icons';
-import UserForm from './UserForm';
+
+const user = {
+  id: 1,
+  userName: 'johnDoe',
+  firstname: 'DOe',
+  email: 'johnede@john.com',
+  role: 'user',
+  isActive: true,
+};
 
 function UsersList() {
+  // const { user } = useUsers();
+  // console.log(user);
+
   return (
-    <div className={`container ${styles.users} `}>
-      {/* {isUsersLoading && "Loading..."}
-			{showUsersForm && <UserForm />} */}
-      <header>
-        <hgroup>
-          <FontAwesomeIcon icon={faClipboardUser} className={styles.icon} />
-          <h2>User details</h2>
-        </hgroup>
-
-        <button className={styles.btnUser}>
-          <FontAwesomeIcon icon={faPen} />
-          <span>Create User</span>
-        </button>
-      </header>
-
-      <table>
-        <thead>
+    <div className="mt-8">
+      <table className="table-auto bg-white w-full">
+        <thead className="bg-bg-light-gray">
           <tr>
-            <th>&nbsp;</th>
-            <th>Username</th>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Update</th>
-            <th>Delete</th>
+            <td className="p-4 rounded-tl-md">Id</td>
+            <td className="p-4">username</td>
+            <td className="p-4">Firstname</td>
+            <td className="p-4">email</td>
+            <td className="p-4">Role</td>
+            <td className="p-4">
+              <span>Account status</span>
+            </td>
+            <td className="p-4">Update</td>
+            <td className="p-4">Delete</td>
           </tr>
         </thead>
         <tbody>
-          {users
-            ? users.map((user) => {
-                return (
-                  <tr key={user.userId}>
-                    <td className={styles.userColId}>{user.userId}</td>
-                    <td>{user.firstName}</td>
-                    <td>{user.lastName}</td>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                    <td>{user.disabled ? 'Disabled' : 'Active'}</td>
-                    <td>
-                      <FontAwesomeIcon
-                        icon={faPenToSquare}
-                        className={styles.iconUpdate}
-                      />
-                    </td>
-                    <td>
-                      <FontAwesomeIcon
-                        icon={faTrashCan}
-                        className={styles.iconDelete}
-                      />
-                    </td>
-                  </tr>
-                );
-              })
-            : ''}
+          <tr className="border border-bg-gray">
+            <td data-th="id" className="p-4 border-r border-bg-gray">
+              {user.id}
+            </td>
+            <td data-th="id" className="p-4 border-r border-bg-gray">
+              {user.userName}
+            </td>
+            <td data-th="id" className="p-4 border-r border-bg-gray">
+              {user.firstname}
+            </td>
+            <td data-th="id" className="p-4 border-r border-bg-gray">
+              {user.email}
+            </td>
+            <td data-th="id" className="p-4 border-r border-bg-gray">
+              {user.role}
+            </td>
+            <td data-th="id" className="p-4 border-r border-bg-gray">
+              {user.isActive ? (
+                <span className="text-dark-green font-medium">Active</span>
+              ) : (
+                <span className="text-color-orange font-medium">Inactive</span>
+              )}
+            </td>
+            <td data-th="id" className="p-4 border-r border-bg-gray">
+              <p className="flex items-center  ">
+                <Button>
+                  <FontAwesomeIcon
+                    icon={faEdit}
+                    className="text-color-secondary"
+                  />
+                </Button>
+                <span>Update user</span>
+              </p>
+            </td>
+            <td data-th="id" className="p-4  border-r border-bg-gray">
+              <p className="flex items-center  ">
+                <Button>
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    className="text-color-orange"
+                  />
+                </Button>
+                <span>Delete user</span>
+              </p>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from '../ui/Button';
+import { faAddressBook, faUserEdit } from '@fortawesome/free-solid-svg-icons';
+import UsersList from '../features/authentication/UsersList';
+
 function Users() {
-  return <div>users page</div>;
+  return (
+    <div className="max-w-[100rem]">
+      <div className="bg-white p-8">
+        <div className="flex justify-between">
+          <div
+            className="flex items-center gap-4 justify-center
+          "
+          >
+            <span>
+              <FontAwesomeIcon icon={faAddressBook} />
+            </span>
+            <h2 className="text-2xl font-normal">User details</h2>
+          </div>
+
+          <Button
+            onClick={onclick}
+            isDisabled=""
+            className=" text-color-primary outline-dashed outline-1 outline-offset-1"
+          >
+            <span>
+              <FontAwesomeIcon icon={faUserEdit} />
+            </span>
+            <p>Create user</p>
+          </Button>
+        </div>
+
+        <UsersList />
+      </div>
+    </div>
+  );
 }
 
 export default Users;
