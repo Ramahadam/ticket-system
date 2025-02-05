30e0df26188ce2a5eebc5635af1372fc303af529
RamahAdam <ramy.adam33@gmail.com>
2025-02-05 14:58:45 +0400

    Add environment variable support for Supabase configuration and update documentation

--- src/apiServices/supabase.js
+++ src/apiServices/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://ljwjpevbbljiavjherqp.supabase.co';
-const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxqd2pwZXZiYmxqaWF2amhlcnFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA3MTkxOTcsImV4cCI6MjAzNjI5NTE5N30.Vffu55LsP4ClSsq2cBk-EMtoCH6tij_51CDr9SlPOWI`;
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
