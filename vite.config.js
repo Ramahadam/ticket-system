7e9a75e136bc226b975411bea81324e969111555
Mohamed <ramy.adam33@gmail.com>
2025-06-29 22:27:07 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.BASE_URL | '/ticket-system',
+  base: proce.env.BASE_URL || '/ticket-system',
   plugins: [react()],
   define: {
     global: 'globalThis', // Define globalThis as an empty object
