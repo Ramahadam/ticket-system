1807192e865bfa367532fda90a5b2ddecf1e05be
Mohamed <ramy.adam33@gmail.com>
2025-06-29 20:28:45 +0400

    Refactor the app - Adujst tialwindcss file - maintian code readablity - remove unnecessary comments

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -27,7 +27,7 @@ function LoginForm() {
           <h1 className="mb-14 text-3xl">Login to your account</h1>
 
           <Input
-            defaultValue="ramy.adam33@gmail.com"
+            defaultValue="adam@itms.com"
             register={register('email', {
               required: 'Email is required',
               pattern: {
